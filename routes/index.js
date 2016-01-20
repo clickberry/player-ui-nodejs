39becf36a43fd778b4fba949ac92ab929ed861ab
kostuyn <kostuyn@gmail.com>
2016-01-20 12:27:29 +0300

    Change protocol setting

--- routes/index.js
+++ routes/index.js
@@ -28,9 +28,10 @@ module.exports = function () {
 
             body = JSON.parse(body);
 
-            var projectId=req.params.projectId;
-            var videoUri = config.get('portal:protocol') + config.get('portal:host') + config.get('portal:videoPath') + '/' + projectId;
-            var playerUri = config.get('portal:protocol') + config.get('portal:playerHost') + config.get('portal:playerPath') + '/' + projectId;
+            var projectId = req.params.projectId;
+            var schema = config.get('portal:protocol') + '://';
+            var videoUri = schema + config.get('portal:host') + config.get('portal:videoPath') + '/' + projectId;
+            var playerUri = schema + config.get('portal:playerHost') + config.get('portal:playerPath') + '/' + projectId;
 
             res.render('index', {
                 title: body.name,
