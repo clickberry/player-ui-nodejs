298ac353f0fecca659840fccc0a220960113b567
kostuyn <kostuyn@gmail.com>
2016-02-01 17:23:41 +0300

    repair

--- routes/index.js
+++ routes/index.js
@@ -22,16 +22,17 @@ module.exports = function () {
                 return next(err);
             }
 
+            var projectId = req.params.projectId;
+            var schema = config.get('portal:protocol') + '://';
+            var portalUri = schema + config.get('portal:host');
+            var videoUri = portalUri + config.get('portal:videoPath') + '/' + projectId;
+            var playerUri = schema + config.get('portal:playerHost') + config.get('portal:playerPath') + '/' + projectId;
+
             if (!response || response.statusCode >= 400) {
-                return res.redirect(301, config.get('portal:uri'))
+                return res.redirect(301, portalUri);
             }
 
             body = JSON.parse(body);
-
-            var projectId = req.params.projectId;
-            var schema = config.get('portal:protocol') + '://';
-            var videoUri = schema + config.get('portal:host') + config.get('portal:videoPath') + '/' + projectId;
-            var playerUri = schema + config.get('portal:playerHost') + config.get('portal:playerPath') + '/' + projectId;
 
             res.render('index', {
                 title: body.name,
